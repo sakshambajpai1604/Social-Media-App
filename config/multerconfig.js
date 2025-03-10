47f434a9b42050bd34a596ce76207e91d05c2142
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-10 10:45:14 +0530

    added the profile pic upload feature

---
+++ config/multerconfig.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const path = require('path');
+const crypto = require('crypto');
+
+// diskstorage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12, function (err, name) {
+            const fn = bytes.toString('hex') + path.extname(file.originalname);
+            cb(null, fn)
+        })
+    }
+})
+
+// export upload variables
+const upload = multer({ storage: storage })
+
+module.exports = upload;
