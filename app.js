ef8039336174853d62b88b982fcbdcf0e7b1830b
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-09-29 16:49:26 +0530

    resolved the No open ports detected issue.

--- app.js
+++ app.js
@@ -130,4 +130,7 @@ function isLoggedIn(req, res, next) {
     }
 }
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
